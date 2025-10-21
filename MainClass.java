ef8f60a7fa0700d6b9d9c6803d8e2af24b567a6f
shoaebansari <76721597+mshoaebansari@users.noreply.github.com>
2025-10-21 09:28:28 +0530

    Two files added from the pc

---
+++ MainClass.java
@@ -0,0 +1,15 @@
+package c;
+import a.*;
+import b.*;
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		a.Example E1=new a.Example();
+		b.Example E2=new b.Example();
+		E1.display();
+		E2.display();
+		E1.checkPrime(30);
+	}
+
+}
